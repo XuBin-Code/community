b06476904ace1036fcf0903e9dae17da30876577
XuBin-Code <xb741734197@163.com>
2019-08-11 11:04:12 +0800

    init repo

---
+++ src/main/java/life/code/xubin/XubinApplication.java
@@ -0,0 +1,14 @@
+package life.code.xubin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude=DataSourceAutoConfiguration.class)
+public class XubinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XubinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/life/code/xubin/XubinApplicationTests.java
@@ -0,0 +1,16 @@
+package life.code.xubin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XubinApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
